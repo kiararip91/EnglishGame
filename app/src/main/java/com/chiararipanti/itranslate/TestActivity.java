2de9af2dea3dd22dfa6fd839bf4dccc481e08ca0
chiararipanti <kiararip91@hotmail.it>
2018-02-07 01:59:23 +0100

    aggiunto pulsante settings

--- app/src/main/java/com/chiararipanti/itranslate/TestActivity.java
+++ app/src/main/java/com/chiararipanti/itranslate/TestActivity.java
@@ -14,6 +14,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -72,6 +73,12 @@ public class TestActivity extends Activity {
         gameUtils = new EnglishGameUtility(this);
         gameUtils.setHomeButtonEnabled();
         gameUtils.addAdBunner();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
     @Override
